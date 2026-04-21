223905efb0b9ce3f28385ba9141664c8740e280c
Muna-Zeer <munaelzeer@gmail.com>
2026-04-21 16:31:49 +0300

    `Update foreign key constraints to use 'cascade' on delete`

--- driving-quiz-backend/database/migrations/2026_04_17_200047_create_levels_table.php
+++ driving-quiz-backend/database/migrations/2026_04_17_200047_create_levels_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('levels', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("category_id")->constrained()->onDelete();
+            $table->foreignId("category_id")->constrained()->onDelete('cascade');
             $table->string('title');
             $table->integer('order')->default(1);
             $table->timestamps();
--- driving-quiz-backend/database/migrations/2026_04_17_200055_create_questions_table.php
+++ driving-quiz-backend/database/migrations/2026_04_17_200055_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('level-id')->constrained()->onDelete();
+            $table->foreignId('level_id')->constrained()->onDelete('cascade');
             $table->string('question_text');
             $table->string('image_url')->nullable();
             $table->timestamps();
