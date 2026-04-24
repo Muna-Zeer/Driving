8a4444e09e3b36d0aadcfe44bccb034f4e1e58b7
Muna-Zeer <munaelzeer@gmail.com>
2026-04-24 22:50:20 +0300

    `Added dependencies for path_provider, shared_preferences, and sqflite`

--- driving_quiz_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ driving_quiz_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e882d64 (`Added dependencies for path_provider, shared_preferences, and sqflite`)
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -14,8 +17,11 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+<<<<<<< HEAD
 =======
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
 >>>>>>> b650c70 (Generate new folders and set up flutter app well)
+=======
+>>>>>>> e882d64 (`Added dependencies for path_provider, shared_preferences, and sqflite`)
 }
