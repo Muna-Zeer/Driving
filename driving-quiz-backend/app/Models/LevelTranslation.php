153595d747749500e12bb558ff301669c3f9f63e
Muna-Zeer <munaelzeer@gmail.com>
2026-05-02 22:37:40 +0300

    Resolving the error in the model of level translation

--- driving-quiz-backend/app/Models/LevelTranslation.php
+++ driving-quiz-backend/app/Models/LevelTranslation.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class LevelTranslation extends Model
 {
     use HasFactory;
-   protected $fillable = [
+
+    protected $fillable = [
         'level_id',
         'locale',
         'name',
         'description'
     ];
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
 }
