a71b4b5326345a50c73cf2838f58b5c0289d1f33
Muna-Zeer <munaelzeer@gmail.com>
2026-05-02 22:26:09 +0300

    Generating level numbers of arabic and english  levels

--- driving-quiz-backend/database/factories/LevelFactory.php
+++ driving-quiz-backend/database/factories/LevelFactory.php
@@ -2,22 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Level;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Level>
- */
 class LevelFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Level::class;
+
     public function definition(): array
     {
+        static $levelNumber = 1;
+
         return [
-            //
+            'group_key' => 'private_driving_theory',
+            'level_number' => $levelNumber++, 
+            'questions_count' => 30,
+            'order' => $levelNumber,
+            'is_active' => true,
         ];
     }
 }
