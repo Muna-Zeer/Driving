4fc257784b47f00d4a695ef916e75cf5532034e4
Muna-Zeer <munaelzeer@gmail.com>
2026-05-02 22:37:40 +0300

    Resolving the error in the model of level translation

--- driving-quiz-backend/app/Models/Level.php
+++ driving-quiz-backend/app/Models/Level.php
@@ -4,8 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\LevelTranslation;
-use App\Models\LevelTranslation as ModelsLevelTranslation;
+
 
 
 
