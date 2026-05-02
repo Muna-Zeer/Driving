2307aa107d55333fcf68af41cf1b60401c1bc240
Muna-Zeer <munaelzeer@gmail.com>
2026-05-02 22:35:12 +0300

    Putting rules of update and store functions of level operation

--- driving-quiz-backend/app/Models/Level.php
+++ driving-quiz-backend/app/Models/Level.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\LevelTranslation;
+use App\Models\LevelTranslation as ModelsLevelTranslation;
 
 
 
@@ -15,7 +17,6 @@ class Level extends Model
         'group_key',
         'level_number',
         'questions_count',
-        'category_id',
         'order',
         'is_active'
     ];
