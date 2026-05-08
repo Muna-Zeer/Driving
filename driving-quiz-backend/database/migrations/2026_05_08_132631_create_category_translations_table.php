08e82394011a3b9478b2abe32c77b7b00e176b4a
Muna-Zeer <munaelzeer@gmail.com>
2026-05-08 16:48:07 +0300

    Generating tables of category and the category translations into db

--- driving-quiz-backend/database/migrations/2026_05_08_132631_create_category_translations_table.php
+++ driving-quiz-backend/database/migrations/2026_05_08_132631_create_category_translations_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('category_translations', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string("locale")->index();
+            $table->string("name");
+            $table->unique(['category_id','locale']);
             $table->timestamps();
         });
     }
