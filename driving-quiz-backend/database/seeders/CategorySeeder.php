14d59faeb7c9f18e567386984e9d25994598d7f0
Muna-Zeer <munaelzeer@gmail.com>
2026-05-08 22:51:38 +0300

    Generating category factory then used and called in the category seeder

--- driving-quiz-backend/database/seeders/CategorySeeder.php
+++ driving-quiz-backend/database/seeders/CategorySeeder.php
@@ -2,16 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        // 1. Generate 20 categories using the factory
+        Category::factory(20)->create()->each(function ($cat) {
+
+
+
+            $cat->translations()->create([
+                'locale' => 'ar',
+                'name'   =>  '  اسم التصنيف  ' . $cat->id,
+            ]);
+
+            $cat->translations()->create([
+                'locale' => 'en',
+                'name'   => 'Category Name ' . $cat->id,
+            ]);
+        });
     }
+
 }
