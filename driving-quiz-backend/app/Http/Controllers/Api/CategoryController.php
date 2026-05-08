7d3c01a6f49303ab4c2e2eda7a2230342dbf77a4
Muna-Zeer <munaelzeer@gmail.com>
2026-05-08 17:31:05 +0300

    Putting the rules and the structure for category response json

--- driving-quiz-backend/app/Http/Controllers/Api/CategoryController.php
+++ driving-quiz-backend/app/Http/Controllers/Api/CategoryController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index():JsonResponse
     {
-        //
+        $categories = Category::with(['translation','levels'])
+        ->where('is_active',true)
+        ->orderBy('order')
+        ->get();
+        return response()->json([
+            'status'=>true,
+            'data'=>CategoryResource::collection($categories)
+        ]);
     }
 
     /**
