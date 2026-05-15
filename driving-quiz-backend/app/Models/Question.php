0534f0d1b22b34e1587316a1d808375bb0f024b8
Muna-Zeer <munaelzeer@gmail.com>
2026-05-16 00:11:38 +0300

    define relationship between question with option and levels

--- driving-quiz-backend/app/Models/Question.php
+++ driving-quiz-backend/app/Models/Question.php
@@ -8,10 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+    protected $fillable = ['level_id','image_url','order','question_text'];
     public function answer(){
         return $this->hasMany(Answer::class);
     }
     public function level(){
         return $this->belongsTo(Level::class);
+    }
+
+    public function options(){
+        return $this->hasMany(Options::class);
+    }
+    public function translations(){
+      return $this->morphMany(QuestionTranslation::class, 'translatable');
     }
 }
