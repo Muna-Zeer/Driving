b163a2299ec902c0e5d56c30e4a69a5eb98ebff7
Muna-Zeer <munaelzeer@gmail.com>
2026-05-16 00:01:44 +0300

    Migrations tables related to question processes

--- driving-quiz-backend/database/migrations/2026_04_17_200055_create_questions_table.php
+++ driving-quiz-backend/database/migrations/2026_04_17_200055_create_questions_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('level_id')->constrained()->onDelete('cascade');
             $table->string('question_text');
             $table->string('image_url')->nullable();
+            $table->integer('order')->default(0);
             $table->timestamps();
         });
     }
