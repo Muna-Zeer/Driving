7fa9364f5f9f7c43bdd77365eeef1b092736ddf7
Muna-Zeer <munaelzeer@gmail.com>
2026-05-17 17:06:35 +0300

    Generate translation resource

--- driving-quiz-backend/app/Http/Resources/TranslationResource.php
+++ driving-quiz-backend/app/Http/Resources/TranslationResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class QuestionResource extends JsonResource
+class TranslationResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,7 +14,10 @@ class QuestionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        
-        return parent::toArray($request);
+
+        return [
+            'locale'=>$this->locale,
+            'text'=>$this->text,
+        ];
     }
 }
