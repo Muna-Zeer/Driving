985675fbee858f0322692a76a96bf472658f7cf1
Muna-Zeer <munaelzeer@gmail.com>
2026-05-18 16:24:45 +0300

    Enhanced the migration of tables related toquestion and options

--- driving-quiz-backend/database/migrations/2026_05_15_204439_create_options_translations_table.php
+++ driving-quiz-backend/database/migrations/2026_05_15_204439_create_options_translations_table.php
@@ -6,25 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
             $table->foreignId('question_id')->constrained()->onDelete('cascade');
             $table->boolean('is_correct')->default(false);
-            $table->char('identifier', 1);
+            $table->char('identifier', 1); 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('options_translations');
+        Schema::dropIfExists('options');
     }
 };
