b3230d40c75a7c8d7e85d49ea5ed95260f0264c5
Muna-Zeer <munaelzeer@gmail.com>
2026-05-18 16:13:30 +0300

    Generate question seeder an factory

--- driving-quiz-backend/database/factories/QuestionFactory.php
+++ driving-quiz-backend/database/factories/QuestionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Level;
+use App\Models\Question;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +16,14 @@ class QuestionFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Question::class;
     public function definition(): array
     {
+       $sampleSigns = ['warning.png', 'stop.png', 'yield.png', 'speed_50.png', 'no_entry.png'];
         return [
-            //
+            'level_id'=>Level::factory(),
+             'image_url'=>$this->faker->boolean(60) ? 'storage/signs/' . $this->faker->randomElement($sampleSigns) : null,
+             'order'=>1
         ];
     }
 }
