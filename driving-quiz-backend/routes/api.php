8ef3f6c6ee366734efd93805b5175bd08c4f6929
Muna-Zeer <munaelzeer@gmail.com>
2026-05-18 17:17:51 +0300

    putting the routes for question process for both quests and admins

--- driving-quiz-backend/routes/api.php
+++ driving-quiz-backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +17,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::post('login', [AuthController::class, 'login']);
-Route::apiResource('level',LevelController::class);
+Route::apiResource('level', LevelController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -31,4 +33,17 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
     Route::post('categories', [CategoryController::class, 'store']);
     Route::put('categories/{id}', [CategoryController::class, 'update']);
     Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+});
+
+
+// Protected routes for question processes(Only Admins can modify)
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('questions', [QuestionController::class, 'index']);
+    Route::get('levels/{level_id}/questions', [QuestionController::class, 'getQuestionByLevel']);
+});
+
+// Public routes (Guests can see categories)
+Route::middleware(['auth:sanctum', 'is_admin'])->prefix('admin')->group(function () {
+    Route::apiResource('questions', QuestionController::class);
 });
