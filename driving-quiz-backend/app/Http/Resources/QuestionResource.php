b304d3d6e9ea0d6d9b2496c46dc9f2b3c6d678b3
Muna-Zeer <munaelzeer@gmail.com>
2026-05-18 16:57:34 +0300

    Enhanced question resource

--- driving-quiz-backend/app/Http/Resources/QuestionResource.php
+++ driving-quiz-backend/app/Http/Resources/QuestionResource.php
@@ -8,22 +8,21 @@ use Vinkla\Hashids\Facades\Hashids;
 
 class QuestionResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         $currentLocale = app()->getLocale();
-        $localizedQuestion = $this->translations->where('locale', $currentLocale)->first()?->text() ?? $this->translations->first()?->text();
+
+        $localizedQuestion = $this->translations->where('locale', $currentLocale)->first()?->text
+            ?? $this->translations->first()?->text;
+
         return [
             'id'         => Hashids::encode($this->id),
             'level_id'   => Hashids::encode($this->level_id),
             'image_url'  => $this->image_url ? url($this->image_url) : null,
             'order'      => (int) $this->order,
             'question'   => $localizedQuestion,
-            'option'     => OptionResource::collection($this->whenLoaded('options')),
+
+            'options'     => OptionResource::collection($this->whenLoaded('options')),
             'all_question_translations' => TranslationResource::collection($this->whenLoaded('translations')),
         ];
     }
