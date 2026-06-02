c394255fa02261c0dfccaa1bdd0474210b823e8b
Muna-Zeer <munaelzeer@gmail.com>
2026-06-02 20:42:36 +0300

    Adding the icons of insert,delete and editing for admin role

--- driving-quiz-backend/app/Http/Resources/LevelResource.php
+++ driving-quiz-backend/app/Http/Resources/LevelResource.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class LevelResource extends JsonResource
 {
-<<<<<<< HEAD
+
     public function toArray(Request $request): array
     {
         $currentLocale = app()->getLocale();
