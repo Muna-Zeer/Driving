6834bc3a3221098b174e978a3b305e7415ae3659
Muna-Zeer <munaelzeer@gmail.com>
2026-06-08 21:44:14 +0300

    resolving the errors in the auth service

--- driving_quiz_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ driving_quiz_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,6 +14,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
