7004c5ea48959018c2502d51d8d1af36c5d36068
Muna-Zeer <munaelzeer@gmail.com>
2026-06-11 21:59:58 +0300

    dependencies

--- driving-quiz-backend/app/Http/Resources/LevelResource.php
+++ driving-quiz-backend/app/Http/Resources/LevelResource.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class LevelResource extends JsonResource
 {
-
     public function toArray(Request $request): array
     {
         $currentLocale = app()->getLocale();
