e19a5a86986aff34a4e1c4e22e00da38b9d4f04e
Muna-Zeer <munaelzeer@gmail.com>
2026-06-15 18:22:02 +0300

    Making the input of category automatically from Laravel without entering by the admin

--- driving-quiz-backend/app/Http/Requests/StoreCategoryRequest.php
+++ driving-quiz-backend/app/Http/Requests/StoreCategoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Category;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCategoryRequest extends FormRequest
@@ -20,11 +21,13 @@ class StoreCategoryRequest extends FormRequest
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
+
     {
+        $nextOrder = Category::max('order')+1;
         return [
             'image_url' => 'nullable|string',
             'type' => 'required|string|max:50',
-            'order' => 'integer',
+            'order' => $nextOrder,
             'is_active' => 'boolean',
             'translations' => 'required|array',
             'translations.*.locale' => 'required|string|max:2',
